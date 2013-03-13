b8f5c0ea136b4306c8cb8804f1a112d2a98f56fb
johnrees <john@bitsushi.com>
2013-03-13 17:49:00 +0100

    series show page

--- spec/features/series_spec.rb
+++ spec/features/series_spec.rb
@@ -6,4 +6,12 @@ describe "Series" do
     visit root_path
     page.should have_content(series.name)
   end
+
+  it "has show page" do
+    series = FactoryGirl.create(:series)
+    visit root_path
+    click_link series.name
+    current_path.should eq(series_path(series))
+    page.should have_content(series.name)
+  end
 end
