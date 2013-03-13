af955a0e55ae92282eead9c54efe2438d01f48b9
johnrees <john@bitsushi.com>
2013-03-13 18:51:50 +0100

    users done for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Lingomics::Application.routes.draw do
 
   resources :comics
-
-
   resources :series
+
+  match 'auth/:provider/callback', to: 'sessions#create'
+  match 'auth/failure', to: redirect('/')
+  match 'signout', to: 'sessions#destroy', as: 'signout'
+
   root to: 'series#index'
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Series.delete_all
+Series.create([
+  { name: 'TinTin' },
+  { name: 'V for Vendetta' },
+  { name: 'Wolverine: First Class' }
+])
